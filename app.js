798eec983382999280e0db084339301bc76924ed
Jani Kaartinen <jkaart@gmail.com>
2025-08-07 11:31:34 +0300

    fix for dotenv import

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import process from 'dotenv'
+import dotenv from 'dotenv'
+
 const app = express()
+
+dotenv.config()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
